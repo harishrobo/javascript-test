f1f489ab6f7a82d1ccc351a0135a8d6385d49e0d
harishrobo <157959685+harishrobo@users.noreply.github.com>
2025-03-28 00:05:15 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -0,0 +1,112 @@
+// let myCountry = "India";
+// let myContinent = "Asia";
+// let myPopulation = "3000 Millions"
+
+// console.log(myCountry);
+// console.log(myContinent);
+// console.log(myPopulation);
+
+// console.log(typeof true);
+
+// let year;
+// console.log(year);
+
+// year = 1995;
+// console.log(year);
+// console.log(typeof year);
+
+
+// let age = "30";
+// age = "31";
+// age = "25"
+
+// const birthDate = "1995";
+// birthDate = "2005";
+
+// console.log(age);
+// console.log(birthDate);
+
+//math operator
+// const ageYear = 2025;
+// const harish = ageYear - 1995;
+// const hema = ageYear - 1997;
+
+// console.log(harish, hema);
+
+// console.log(harish * 2, harish / 10, 2 ** 3)
+// // 2**3 means, 2 to the power of 3
+
+// const firstName = "harish";
+// const lastName = "kumar";
+
+// console.log(firstName + ' ' + lastName);
+
+// // Assigment operator
+// let x = 15; // 15
+// x += 10; // x = x + 10
+// console.log(x);
+
+// //Comparison operator "> - true or false"
+// console.log(harish > hema);
+// console.log(harish >= hema);
+
+//strings & variables -
+
+// const firstNamee = "Harish";
+// const birthAge = 1995;
+// const currentYear = 2025;
+
+// const Harish = "Im " + firstNamee + " my age is " + (currentYear - birthAge);
+// console.log(Harish);
+
+// // CHECK *Template Literals {} - ${firstNamee}*
+// const lapname = `Im ${firstNamee}, my age is ${currentYear - birthAge}`;
+// console.log(lapname);
+
+// //even we can use the strings in console
+// console.log(`Hello Harish`);
+
+
+
+//*****************
+//IF ELSE STARTS
+
+// const age = 12;
+
+// if (age >= 18) {
+//     console.log('Hi, you can drive license');
+// } else {
+//     const waitingYear = 18 - age;
+//     console.log(`your age is ${waitingYear} so you cannot get license`);
+// }
+
+
+//Test COde
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+// if (BMIJohn > BMIMark) {
+//     console.log(`John's BMI is higher than Mark's!`)
+//     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`)
+// } else {
+//     console.log(`Mark's BMI is higher than John's!`)
+//     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`)
+// }
+
+//Mark's BMI (28.3) is higher than John's (23.9)!
+
+
+// == or === Operators
+const age = 18;
+
+if (age === 18) {
+    console.log("Its true");
+}
